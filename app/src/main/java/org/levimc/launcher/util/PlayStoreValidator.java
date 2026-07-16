44bbd2788792f2c2c5fd38b42b8e85da77757b49
Christianzz4603 <christianjaydelica3@gmail.com>
2026-07-16 18:55:24 +0800

    Initial commit

---
+++ app/src/main/java/org/levimc/launcher/util/PlayStoreValidator.java
@@ -0,0 +1,7 @@
+package org.levimc.launcher.util;
+
+import android.content.Context;
+
+public class PlayStoreValidator {
+    public static boolean isMinecraftFromPlayStore(Context context) { return true; }
+}
